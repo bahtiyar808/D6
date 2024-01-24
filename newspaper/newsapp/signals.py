f0d27cc677aa30035043b42d1222903affe79c26
Bahtiyar <bahtiyar_t9581@list.ru>
2024-01-24 23:07:43 +0300

    добавлены рассылки о новых материалах в категориях, реализована отправка списка статей каждую неделю

--- newspaper/newsapp/signals.py
+++ newspaper/newsapp/signals.py
@@ -2,17 +2,18 @@ from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives, send_mail
 from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
+from django.template.loader import render_to_string
 
 from .models import PostCategory
 from django.conf import settings
 
 
 def send_notifications(preview, pk, title, subscribers):
-    html_content = (
+    html_content = render_to_string(
         'post_created_email.html',
         {
             'text': preview,
-            'link': f'http://127.0.0.1:8000/news/{pk}'
+            'link': f'http://127.0.0.1:8000/newspaper/{pk}'
         }
     )
     msg = EmailMultiAlternatives(
